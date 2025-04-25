3dd5791fbe77409c479ff6addaf65becc84f9209
eric-2203 <eric2203@outlook.es>
2025-04-25 15:55:36 -0600

    updated exercise 2

--- Semana11/exercise_2.py
+++ Semana11/exercise_2.py
@@ -13,23 +13,24 @@ class Bus:
 
     def get_people(self, passenger):
         if len(self.passengers) < self.max_passengers: 
-            self.passengers.append(passenger)
+            self.passengers.append(passenger.name)
         else:
-            print("The bus is full, please wait for the next one.")
+            print(f"{passenger.name} the bus is full, please wait for the next one.")
 
     def remove_people(self, passenger):
         number = random.randint(0, self.max_passengers)
         self.passengers.pop(number)
+            
         
 
 person1 = Person("Eric")
 person2 = Person("Dani")
 person3 = Person("Alice")
 
-my_bus = Bus(4)
+my_bus = Bus(2)
 my_bus.get_people(person1)
 my_bus.get_people(person2)
 my_bus.get_people(person3)
 
 
-print(my_bus.passengers)
+print(f"People on the bus: {my_bus.passengers}")
