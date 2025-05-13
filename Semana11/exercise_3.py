88dddf5df06dd7088a3b6c725e6f17b4abe6367a
eric-2203 <eric2203@outlook.es>
2025-05-12 19:04:51 -0600

    updates on exercise 3

--- Semana11/exercise_3.py
+++ Semana11/exercise_3.py
@@ -19,20 +19,61 @@ class Student():
         self.average = (self.spanish + self.english + self.science + self.socials) / 4
         print(f"Average score: {self.average}")
 
-name = input("Student name: ")
-section = input("Section: ")
-spanish_score = int(input("Spanish Score: "))
-english_score = int(input("English Score: "))
-science_score = int(input("Science Score: "))
-socials_score = int(input("Socials Score: "))
 
-students_list = []
-while True:
-    students = Student(name, section, spanish_score, english_score, science_score, socials_score)
-    students_list.append(students)
-    more_info = input("Do you want to add more students information? (yes/no): ")
-    if more_info.lower() != 'yes':
-        break
+def get_student_info():
+    global details
+    global average
+    global all_scores
+    
+    average = []
+    all_scores = []
+
+    name = input("Student name: ")
+    section = input("Section: ")
+    spanish_score = int(input("Spanish Score: "))
+    english_score = int(input("English Score: "))
+    science_score = int(input("Science Score: "))
+    socials_score = int(input("Socials Score: "))
+    details = [name, section, spanish_score, english_score, science_score, socials_score]
+    average_scores = (spanish_score + english_score + science_score + socials_score)/4
+
+    top_students = average_scores
+
+    all_scores.append(spanish_score)
+    all_scores.append(english_score)
+    all_scores.append(science_score)
+    all_scores.append(socials_score)
+
+    ranking = [name, top_students]
+    average.append(ranking)
 
 
-students.show_details()
+    return Student
+
+
+def register_student():
+    global students
+    students = []
+
+    while True:
+        student = get_student_info()
+        students.append(details)
+
+        
+        more_info = input("Do you want to add more students information? (yes/no): ")
+        if more_info.lower() != 'yes':
+            break
+        
+    return students
+
+def show_top_3():
+    sorted_average = sorted(average, key=lambda scores: scores[1], reverse=True)
+    top_3 = sorted_average[:3]
+
+    print(f'These are the top 3 students with their average score: {top_3}')
+
+
+
+register_student()
+print(students)
+show_top_3()
