62b1c0b06d09dedddbca97512dec15662d1d4d30
eric-2203 <eric2203@outlook.es>
2025-10-30 18:35:13 -0600

    updates required by system

--- Semana15/analisis_algoritmos_1.py
+++ Semana15/analisis_algoritmos_1.py
@@ -15,7 +15,7 @@ def bubble_sort(list_to_sort):
 
         if not changes_made: # O(1)
             return  # O(1)
-test_list = [3, 4, 5, 6, 8, 7]   # O(1)"
+test_list = [3, 4, 5, 6, 8, 7]   # O(1)
 
 bubble_sort(test_list)  # O(1)
 
