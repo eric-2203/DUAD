3469118ffa96eebf45d7c934f352ede574c3c85a
eric-2203 <eric2203@outlook.es>
2026-04-28 19:02:39 -0600

    adding updates

--- Flask/main.py
+++ Flask/main.py
@@ -1,8 +1,6 @@
 #Crea un API con Flask de que permita un CRUD (Create, Read, Update, Delete) de tareas. 
-# Importé todas las librerías que vi en el video, en caso de necesitarlas.
-from flask import Flask, jsonify, request, Response, url_for, session, redirect
+from flask import Flask, jsonify, request
 import json
-from dataclasses import dataclass
 
 app = Flask(__name__)
 
@@ -10,7 +8,7 @@ app = Flask(__name__)
 def root():
     return "<h1>Bienvenido al API de Tareas!</h1>"
 
-valid_status = ["por hacer", "en progreso", "completada"]
+valid_status = ["pending", "in progress", "completed"]
 
 def write_file(tasks):
     with open('tasks.json', 'w', encoding='utf-8') as file:
@@ -26,81 +24,82 @@ def read_file():
     return tasks
 
 
-@app.route("/tareas")
+@app.route("/tasks", methods=['GET'])
 def show_tasks():
     tasks = read_file()
-    tasks_filter = request.args.get("estado")
+    tasks_filter = request.args.get("status")
     if tasks_filter:
         tasks = list(
-            filter(lambda task: task["estado"] == tasks_filter, tasks)
+            filter(lambda task: task["status"] == tasks_filter.lower(), tasks)
         )
-    return {"data": tasks}
+    return jsonify(tasks)
 
-@app.route("/register", methods=['POST'])
+@app.route("/tasks", methods=['POST'])
 def register_task():
     tasks = read_file()
     try:
-        if "identificador" not in request.json or request.json["identificador"] == "":
+        if "identifier" not in request.json or request.json["identifier"] == "":
             raise ValueError("Hace falta el identificador de la tarea.")
         
-        if "titulo" not in request.json or request.json["titulo"] == "":
+        if "title" not in request.json or request.json["title"] == "":
             raise ValueError("Hace falta el titulo de la tarea.")
         
-        if "descripcion" not in request.json or request.json["descripcion"] == "":
+        if "description" not in request.json or request.json["description"] == "":
             raise ValueError("Hace falta la descripcion de la tarea.")
         
-        if "estado" not in request.json or request.json["estado"] == "":
+        if "status" not in request.json or request.json["status"] == "":
             raise ValueError("Hace falta el estado de la tarea.")
-        if request.json["estado"].lower() not in valid_status:
+        if request.json["status"].lower() not in valid_status:
             raise ValueError("El estado ingresado no es valido. Ingrese un estado valido.")
         
         for task in tasks:
-            if task["identificador"] == request.json["identificador"]:
+            if task["identifier"] == request.json["identifier"]:
                 raise ValueError("Este identificador de tarea ya existe. Inserta un identificador nuevo.")
 
         tasks.append(
             {
-                "identificador": request.json["identificador"],
-                "titulo": request.json["titulo"],
-                "descripcion": request.json["descripcion"],
-                "estado": request.json["estado"].lower()
+                "identifier": request.json["identifier"],
+                "title": request.json["title"],
+                "description": request.json["description"],
+                "status": request.json["status"].lower()
             }
         )
 
         write_file(tasks)
-        return tasks
+        return jsonify(tasks), 201
     except ValueError as ex:
         return jsonify(message=str(ex)), 400
     except Exception as ex:
         return jsonify(message=str(ex)), 500
     
     
-@app.route("/edit", methods=['PUT'])
-def edit_task():
+@app.route("/tasks/<id>", methods=['PUT'])
+def edit_task(id):
     tasks = read_file()
     found = False
     try: 
-        if "titulo" not in request.json or request.json["titulo"] == "":
+
+        if "title" not in request.json or request.json["title"] == "":
             raise ValueError("Hace falta el titulo de la tarea.")
         
-        if "descripcion" not in request.json or request.json["descripcion"] == "":
+        if "description" not in request.json or request.json["description"] == "":
             raise ValueError("Hace falta la descripcion de la tarea.")
         
-        if "estado" not in request.json or request.json["estado"] == "":
+        if "status" not in request.json or request.json["status"] == "":
             raise ValueError("Hace falta el estado de la tarea.")
-        if request.json["estado"].lower() not in valid_status:
+        if request.json["status"].lower() not in valid_status:
             raise ValueError("El estado ingresado no es valido. Ingrese un estado valido.")
         
         for task in tasks:
-            if task["identificador"] == request.json["identificador"]:
-                task["titulo"] = request.json["titulo"]
-                task["descripcion"] = request.json["descripcion"]
-                task["estado"] = request.json["estado"].lower()
+            if task["identifier"] == id:
+                task["title"] = request.json["title"]
+                task["description"] = request.json["description"]
+                task["status"] = request.json["status"].lower()
                 found = True
                 break
         if found is True:
             write_file(tasks)
-            return tasks
+            return jsonify(tasks)
         elif found == False:
             raise ValueError("El identificador de tarea no existe o enviaste un valor vacío.")
 
@@ -111,19 +110,19 @@ def edit_task():
         return jsonify(message=str(ex)), 500
 
 
-@app.route("/delete", methods=['DELETE'])
-def delete_task():
+@app.route("/tasks/<id>", methods=['DELETE'])
+def delete_task(id):
     tasks = read_file()
     found = False
     try:  
         for task in tasks:
-            if task["identificador"] == request.json["identificador"]:
+            if task["identifier"] == id:
                 tasks.remove(task)
                 found = True
                 break
         if found is True:
             write_file(tasks)
-            return "Tarea eliminada de manera exitosa."
+            return jsonify(tasks)
         elif found is False:
             raise ValueError("El identificador de tarea no existe o enviaste un valor vacío.")
     
